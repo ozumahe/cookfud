7788b6c65bf4e355b0245217a1df4d01f76cc53c
Ozumah Ebenezer <ozumahe@gmail.com>
2022-10-22 13:44:15 +0100

    Update

--- src/app/pages/foods/foods.component.ts
+++ src/app/pages/foods/foods.component.ts
@@ -8,18 +8,37 @@ import apiCall from '../../utils/apiCall';
 })
 export class FoodsComponent implements OnInit {
   foods: any = { loaded: false, data: [] };
+  category: string = '';
+  categories: any = { loaded: false, data: [] };
+
+  selected = '';
 
   constructor() {}
 
   ngOnInit(): void {
     this.getFoods();
+    this.getCategories();
   }
 
   async getFoods() {
-    const res = await apiCall({ url: `search.php?s=`, method: 'GET' });
+    const res = await apiCall({
+      url: `search.php?s=${this.selected ? this.selected : ''}`,
+      method: 'GET',
+    });
 
     if (res.status === 200) {
       this.foods = { loaded: true, data: res.data.meals };
+    }
+  }
+
+  async getCategories() {
+    const res = await apiCall({
+      url: `categories.php`,
+      method: 'GET',
+    });
+
+    if (res.status === 200) {
+      this.categories = { loaded: true, data: res.data.categories };
     }
   }
 }
